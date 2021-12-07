30959a2ee2ad2c729482c7365fa28bc33591b172
Aarjion <lfybtkm37@mail.ru>
2021-12-07 13:18:33 +0500

    design front 100% +5%

--- tensor_project/app/static/counter.js
+++ tensor_project/app/static/counter.js
@@ -1,9 +1,24 @@
-var vue = new Vue ({
-    el: "#app",
-    data: {
-      text: 'Hi Kate !!!' 
-    },
-    methods: {
+const div = document.getElementById('room');
+
+function dublicateForm(){
+  let forms = div.getElementsByClassName('room_renovation');
+  let firstForm = forms[0];
+  let formClone = firstForm.cloneNode(true);
+  div.appendChild(formClone);
+}
+
+
+
+
+
+
+
+// var vue = new Vue ({
+//     el: "#app",
+//     data: {
+//       text: 'Hi Kate !!!' 
+//     },
+//     methods: {
    
-    }
-});
+//     }
+// });
